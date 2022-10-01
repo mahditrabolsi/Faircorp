5a7d8564860e583c0e34ed266c24e02488872e9d
mahditrabolsi <mahditrabolsi@gmail.com>
2022-10-01 11:05:33 +0200

    Added Dependencies

--- src/main/java/com/emse/spring/faircorp/FaircorpApplication.java
+++ src/main/java/com/emse/spring/faircorp/FaircorpApplication.java
@@ -2,12 +2,14 @@ package com.emse.spring.faircorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 public class FaircorpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FaircorpApplication.class, args);
+
 	}
 
 }
